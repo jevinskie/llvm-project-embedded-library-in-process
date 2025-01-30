abbbb2aa84a074ddfc794cd93353e339a1a62952
Lucas Duarte Prates <lucas.prates@arm.com>
2025-01-30 09:26:58 +0000

    [ATfE] Add scripts to build newlib and llvmlibc overlay packages (#63)
    This introduces two new scripts for building ovelay library packages for
    the Arm Toolchain for Embedded, one for the newlib overlay and one for
    the llvmlibc overlay.
    The scripts follow the same approach as the existing `build.sh` script,
    but using different build directories and providing the corresponding
    CMake options for each overlay package.

---
+++ arm-software/embedded/scripts/build_newlib_overlay.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# A bash script to build the newlib overlay for the Arm Toolchain for Embedded
+
+# The script creates a build of the toolchain in the 'build_newlib_overlay'
+# directory, inside the repository tree.
+
+set -ex
+
+export CC=clang
+export CXX=clang++
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+REPO_ROOT=$( git -C ${SCRIPT_DIR} rev-parse --show-toplevel )
+BUILD_DIR=${REPO_ROOT}/build_newlib_overlay
+
+mkdir -p ${BUILD_DIR}
+cd ${BUILD_DIR}
+
+cmake ../arm-software/embedded -GNinja -DFETCHCONTENT_QUIET=OFF -DLLVM_TOOLCHAIN_C_LIBRARY=newlib -DLLVM_TOOLCHAIN_LIBRARY_OVERLAY_INSTALL=on
+ninja package-llvm-toolchain
+
+# The package-llvm-toolchain target will produce a .tar.xz package, but we also
+# a zip version for Windows users
+cpack -G ZIP
