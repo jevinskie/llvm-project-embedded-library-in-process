4430235076d8f5a0728e0fcf54fa3897845c1e45
Victor Campos <victor.campos@arm.com>
2025-02-03 09:05:05 +0000

    Filter Corstone's stdout (#61)
    Corstone prints out some informational text to stdout, but this text
    conflicts with expectations from tests about the contents of stdout.
    
    This patch filters out this text to send to stdout only what matters.

--- arm-software/embedded/arm-runtimes/test-support/run_fvp.py
+++ arm-software/embedded/arm-runtimes/test-support/run_fvp.py
@@ -2,6 +2,7 @@
 
 # SPDX-FileCopyrightText: Copyright 2024 Arm Limited and/or its affiliates <open-source-office@arm.com>
 
+import re
 import subprocess
 import sys
 from os import path
@@ -92,6 +93,38 @@ def run_fvp(
         cwd=working_directory,
         check=False,
     )
-    sys.stdout.buffer.write(result.stdout)
-    return result.returncode
 
+    # Corstone-310 prints out boilerplate text on stdout alongside the actual
+    # output of the image. Some tests, for instance in libcxx, check the
+    # contents of stdout, and may treat the unexpected text as a condition for
+    # failure. To work around this, we cut out the model's boilerplate output.
+    if fvp_model == "corstone-310":
+        decoded_stdout = result.stdout.decode()
+        expected_stdout_format = r"""
+    Ethos-U rev [0-9a-z]+ --- \w{3} \d{2} \d{4} \d{2}:\d{2}:\d{2}
+    \(C\) COPYRIGHT (?:\d{4}|\d{4}-\d{4})(?:,\s?(?:\d{4}|\d{4}-\d{4}))* Arm Limited
+    ALL RIGHTS RESERVED
+
+(.*)
+Info: /OSCI/SystemC: Simulation stopped by user.
+\[warning \]\[main@0\]\[\d+ ns\] Simulation stopped by user
+"""
+
+        regex_result = re.fullmatch(
+            expected_stdout_format, decoded_stdout, flags=re.DOTALL
+        )
+        if not regex_result:
+            error_msg = (
+                f"Corstone's output format is different than expected\n"
+                f"Expected (regex): {expected_stdout_format}\n"
+                f"Got: {decoded_stdout}"
+            )
+            raise RuntimeError(error_msg)
+
+        relevant_stdout = regex_result[1]
+        result_stdout = relevant_stdout.encode()
+    else:
+        result_stdout = result.stdout
+
+    sys.stdout.buffer.write(result_stdout)
+    return result.returncode
